5392102778f7e375542411886aff7e1d3b6c7947
FelipeAlvarado01 <felipeAF2001alvarado@gmail.com>
2023-04-28 03:03:48 -0500

    muestran los productos que son guardados en "BD"

--- frontend/src/assets/js/productsDates.js
+++ frontend/src/assets/js/productsDates.js
@@ -1,54 +1,19 @@
-let url='https://catalogopuntomotero-production.up.railway.app/api/create';//crear productos 
 let productId = 1;
+let productos = JSON.parse(localStorage.getItem("productos")) || [];
+
+if (productos.length > 0) {
+  // Obtener el último id asignado
+  productId = productos[productos.length - 1].id + 1;
+}
+
 let capturaProducto = document.getElementById("formCrear");
 
 capturaProducto.addEventListener("submit", function (event) {
   event.preventDefault();
-  /*const enviarJson = {};
-  enviarJson.nombre = document.querySelector("#nombre").value;
-
-  let selectCategoria = document.querySelector('#categoria');
-  let documentoSelected = selectCategoria.selectedOptions[0];
-  enviarJson.categoria = documentoSelected.textContent;
-
-  enviarJson.imagen = document.querySelector("#imagen").value;
-  enviarJson.descripcion = document.querySelector("#descripcion").value;
-  enviarJson.precio = Number(document.querySelector("#precio").value);
-  console.log(enviarJson);*/
-  /*let selectCategoria = document.querySelector('#categoria');
-  let documentoSelected = selectCategoria.selectedOptions[0];
-
-  const data = {
-    nombre: document.querySelector('#nombre').value,
-    categoria: documentoSelected.textContent,
-    imagen : document.querySelector("#imagen").value,
-    descripcion : document.querySelector("#descripcion").value,
-    precio : Number(document.querySelector("#precio").value)
-  };
-
-  console.log(data);
-
-  fetch('https://catalogopuntomotero-production.up.railway.app/create', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.log("El producto no se puedo cargar");
-  });*/
 
   let selectCategoria = document.querySelector('#categoria');
   let documentoSelected = selectCategoria.selectedOptions[0];
   
-  //let file = capturaProducto.imagen.files[0];
-  //let reader = new FileReader();
-
   const data = {
     id: productId,
     nombre: document.querySelector('#nombre').value,
@@ -58,12 +23,9 @@ capturaProducto.addEventListener("submit", function (event) {
     precio : Number(document.querySelector("#precio").value)
   };
 
-  let productos = JSON.parse(localStorage.getItem("productos")) || [];
   productos.push(data);
   localStorage.setItem("productos", JSON.stringify(productos));
-
   productId++;
 
   alert("Los datos se han guardado correctamente en el LocalStorage.");
-
 });
--- frontend/src/assets/js/scrollBehavior.js
+++ frontend/src/assets/js/scrollBehavior.js
@@ -4,11 +4,65 @@ const leftButton = document.querySelector("#scrolling-button_left");
 const content = document.querySelector(".scrolling-container");
 
 //Para mover el contenido a la derecha
-
-rightButton.addEventListener("click",()=>{
+rightButton.addEventListener("click", () => {
   content.scrollLeft += 800; //Movemos 800px a la derecha
 });
 
-leftButton.addEventListener("click",()=>{
+leftButton.addEventListener("click", () => {
   content.scrollLeft -= 800; //Movemos 800px a la izquierda
-})
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Obtener los datos del local storage
+let productos = JSON.parse(localStorage.getItem("productos"));
+let categoria;
+let contenedorPrincipal = document.getElementById("productos");
+
+// Filtrar los productos por categoría
+const cards = document.querySelectorAll('.scrolling-card');
+cards.forEach(card => {
+  card.addEventListener('click', (event) => {
+    event.preventDefault();
+    categoria = card.dataset.categoria;
+    mostrarProductosxCategoria(categoria);
+  });
+});
+
+function mostrarProductosxCategoria(categoria) {
+  console.log(categoria);
+
+  let productosFiltrados = productos.filter((producto) => producto.categoria === categoria);
+
+  // Limpiar el contenedor principal
+  contenedorPrincipal.innerHTML = "";
+
+  // Crear un nuevo array con los productos filtrados
+  let productosPorFila = [];
+  for (let i = 0; i < productosFiltrados.length; i += 3) {
+    let fila = productosFiltrados.slice(i, i + 3);
+    productosPorFila.push(fila);
+  }
+
+  // Recorrer el array de productos por fila y agregarlos a la página
+  productosPorFila.forEach((fila) => {
+    let contenedorFila = document.createElement("div");
+    contenedorFila.classList.add("product-container");
+    contenedorFila.classList.add("fila");
+    fila.forEach((producto) => {
+      let elementoProducto = document.createElement("article");
+      elementoProducto.classList.add("article-product");
+      elementoProducto.classList.add("producto");
+      elementoProducto.innerHTML = `
+      <img src="${producto.imagen}">
+      <div class="product-description">
+      <h4>${producto.nombre}</h4>
+      <p class="description-product">${producto.descripcion}</p>
+      <p class="cost-product">Precio: $${producto.precio}</p>
+      </div>
+    `;
+      contenedorFila.appendChild(elementoProducto);
+    });
+    contenedorPrincipal.appendChild(contenedorFila);
+  });
+}
+
