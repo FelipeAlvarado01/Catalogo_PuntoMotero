e778da02efbdc64e8b86a8ace218671a081a435c
FelipeAlvarado01 <felipeAF2001alvarado@gmail.com>
2023-04-28 00:43:55 -0500

    consultas desde el "backend"

--- frontend/src/assets/js/productsDates.js
+++ frontend/src/assets/js/productsDates.js
@@ -1,12 +1,10 @@
 let url='https://catalogopuntomotero-production.up.railway.app/api/create';//crear productos 
-
+let productId = 1;
 let capturaProducto = document.getElementById("formCrear");
 
 capturaProducto.addEventListener("submit", function (event) {
   event.preventDefault();
-
   /*const enviarJson = {};
-
   enviarJson.nombre = document.querySelector("#nombre").value;
 
   let selectCategoria = document.querySelector('#categoria');
@@ -17,7 +15,7 @@ capturaProducto.addEventListener("submit", function (event) {
   enviarJson.descripcion = document.querySelector("#descripcion").value;
   enviarJson.precio = Number(document.querySelector("#precio").value);
   console.log(enviarJson);*/
-  let selectCategoria = document.querySelector('#categoria');
+  /*let selectCategoria = document.querySelector('#categoria');
   let documentoSelected = selectCategoria.selectedOptions[0];
 
   const data = {
@@ -43,5 +41,29 @@ capturaProducto.addEventListener("submit", function (event) {
   })
   .catch(error => {
     console.log("El producto no se puedo cargar");
-  });
+  });*/
+
+  let selectCategoria = document.querySelector('#categoria');
+  let documentoSelected = selectCategoria.selectedOptions[0];
+  
+  //let file = capturaProducto.imagen.files[0];
+  //let reader = new FileReader();
+
+  const data = {
+    id: productId,
+    nombre: document.querySelector('#nombre').value,
+    categoria: documentoSelected.textContent,
+    imagen : document.querySelector('#imagen').value,
+    descripcion : document.querySelector("#descripcion").value,
+    precio : Number(document.querySelector("#precio").value)
+  };
+
+  let productos = JSON.parse(localStorage.getItem("productos")) || [];
+  productos.push(data);
+  localStorage.setItem("productos", JSON.stringify(productos));
+
+  productId++;
+
+  alert("Los datos se han guardado correctamente en el LocalStorage.");
+
 });
